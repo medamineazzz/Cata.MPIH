fb74dafcf2310750d839d0018db60e23dfd8fdf9
medamineazzz <amineazzouzi16@gmail.com>
2025-06-11 13:49:51 +0100

    Update moroccan_gps_insight_hub_clean.py

--- moroccan_gps_insight_hub_clean.py
+++ moroccan_gps_insight_hub_clean.py
@@ -9,7 +9,10 @@ st.set_page_config(page_title="Moroccan GPS Insight Hub", layout="centered")
 
 # App Config
 # Moroccan FA Logo + Custom Title
-st.image("https://e1.pngegg.com/pngimages/536/1022/png-clipart-logo-de-football-coupe-du-monde-2018-maroc-equipe-nationale-de-football-du-maroc-federation-royale-marocaine-de-football-equipe-nationale-de-football-des-moins-de-20-ans-du-maroc-thumbnail.png", width=120)
+st.image(
+    "https://upload.wikimedia.org/wikipedia/fr/thumb/6/69/Logo_F%C3%A9d%C3%A9ration_Royale_Marocaine_Football.svg/1507px-Logo_F%C3%A9d%C3%A9ration_Royale_Marocaine_Football.svg.png",
+    width=80
+)
 
 st.markdown("""
     <h1 style='text-align: center; color: #d32f2f;'> Moroccan GPS Insight Hub</h1>
